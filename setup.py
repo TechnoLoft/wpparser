90367fdd07c755bad8c320e9c991089649e77891
Ryan Jarvis <828557+Cabalist@users.noreply.github.com>
2026-02-20 13:48:56 -0800

    Fix build process by updating setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 
-import os
 import re
-import sys
 from setuptools import find_packages, setup
-
-import wpparser
-
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
-
-
-test_requirements = [
-    "pytest>=3",
-]
 
 requires = [
     "phpserialize>=1.3",
@@ -26,27 +13,23 @@ with open("wpparser/__init__.py", "r") as fd:
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
     ).group(1)
 
-# Convert markdown to rst
-try:
-    from pypandoc import convert_file
-    long_description = convert_file("README.md", "rst")
-except:
-    long_description = ""
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name="wpparser",
     version=version,
-    description="Parse wordpress export files into a well formatted python dictionary",  # NOQA
+    description="Parse wordpress export files into a well formatted python dictionary",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Martin Sandström",
     author_email="martin@marteinn.se",
     url="https://github.com/marteinn/wpparser",
     packages=find_packages(),
-    package_data={"": ["LICENSE", ], "wpparser": ["*.txt"]},
+    package_data={"": ["LICENSE"], "wpparser": ["*.txt"]},
     package_dir={"wpparser": "wpparser"},
     include_package_data=True,
     install_requires=requires,
-    tests_require=test_requirements,
     license="MIT",
     zip_safe=False,
     classifiers=[
